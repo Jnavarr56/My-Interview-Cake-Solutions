de56b2f5379bd58f8f1656b5f1cfbd7bb36509fa
Jorge Navarro <jnavarr56@gmail.com>
2019-03-15 10:55:25 -0400

    first commit

---
+++ Merge-Meeting-Times/mergeMeetingTimes.js
@@ -0,0 +1,104 @@
+const mergeRanges = (mtgs) => {
+
+  mtgs.sort((a, b) => a.startTime - b.startTime);
+
+  console.log(mtgs);
+
+  let mgd = [], mtgData;
+
+  mtgs.reduce((lMtg, cMtg, i, mtgs) => {
+
+    mtgData = cMtg.startTime >= lMtg.startTime && cMtg.startTime <= lMtg.endTime ? {mgd: true,  mtg: {startTime: lMtg.startTime, endTime: Math.max(lMtg.endTime, cMtg.endTime)}} : {mgd: false,  mtg: cMtg};
+
+    if (!mtgData.mgd) mgd.push(lMtg);
+
+    if (i === mtgs.length - 1) mgd.push(mtgData.mtg);
+
+    return mtgData.mtg;
+
+  });
+
+  console.log(mgd);
+
+  return mgd;
+  
+}
+
+
+// Tests
+
+let desc = 'meetings overlap';
+let actual = mergeRanges([{ startTime: 1, endTime: 3 }, { startTime: 2, endTime: 4 }]);
+let expected = [{ startTime: 1, endTime: 4 }];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'meetings touch';
+actual = mergeRanges([{ startTime: 5, endTime: 6 }, { startTime: 6, endTime: 8 }]);
+expected = [{ startTime: 5, endTime: 8 }];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'meeting contains other meeting';
+actual = mergeRanges([{ startTime: 1, endTime: 8 }, { startTime: 2, endTime: 5 }]);
+expected = [{ startTime: 1, endTime: 8 }];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'meetings stay separate';
+actual = mergeRanges([{ startTime: 1, endTime: 3 }, { startTime: 4, endTime: 8 }]);
+expected = [{ startTime: 1, endTime: 3 }, { startTime: 4, endTime: 8 }];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'multiple merged meetings';
+actual = mergeRanges([
+  { startTime: 1, endTime: 4 },
+  { startTime: 2, endTime: 5 },
+  { startTime: 5, endTime: 8 },
+]);
+expected = [{ startTime: 1, endTime: 8 }];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'meetings not sorted';
+actual = mergeRanges([
+  { startTime: 5, endTime: 8 },
+  { startTime: 1, endTime: 4 },
+  { startTime: 6, endTime: 8 },
+]);
+expected = [{ startTime: 1, endTime: 4 }, { startTime: 5, endTime: 8 }];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'oneLongMeetingContainsSmallerMeetings';
+actual = mergeRanges([
+  { startTime: 1, endTime: 10 },
+  { startTime: 2, endTime: 5 },
+  { startTime: 6, endTime: 8 },
+  { startTime: 9, endTime: 10 },
+  { startTime: 10, endTime: 12 }
+]);
+expected = [
+  { startTime: 1, endTime: 12 }
+];
+assertArrayEquals(actual, expected, desc);
+
+desc = 'sample input';
+actual = mergeRanges([
+  { startTime: 0, endTime: 1 },
+  { startTime: 3, endTime: 5 },
+  { startTime: 4, endTime: 8 },
+  { startTime: 10, endTime: 12 },
+  { startTime: 9, endTime: 10 },
+]);
+expected = [
+  { startTime: 0, endTime: 1 },
+  { startTime: 3, endTime: 8 },
+  { startTime: 9, endTime: 12 },
+];
+assertArrayEquals(actual, expected, desc);
+
+function assertArrayEquals(a, b, desc) {
+  const arrayA = JSON.stringify(a);
+  const arrayB = JSON.stringify(b);
+  if (arrayA !== arrayB) {
+    console.log(`${desc} ... FAIL: ${arrayA} != ${arrayB}`)
+  } else {
+    console.log(`${desc} ... PASS`);
+  }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+### My Solutions to InterviewCake's Course Interview Practice Problems
+##### I include implementations in:
+- JavaScript
+- Ruby
+- Java
+
